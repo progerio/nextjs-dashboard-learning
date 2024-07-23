40126a8d6ffc2ecccd0d44aa2706874f109bc3ba
Paulo Rogerio <paulo@intranet>
2024-07-23 11:20:17 -0300

    Aprendendo capitulo 12 - delete fatura

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -16,14 +17,10 @@ const CreateInvoice  = FormSchema.omit({
     id: true, date: true
 });
 
-// Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
 
 export async function createInvoice(formData: FormData) {
 const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData.entries()));
-  // Test it out:
-  
   const amountInCents = amount * 100;
   const date  = new Date().toISOString().split('T')[0];
 
@@ -40,8 +37,12 @@ export async function updateInvoice(id: string, formData: FormData) {
   
   const amountInCents = amount * 100;
   
-
   await sql`UPDATE invoices SET status = 'paid' WHERE id = ${id}`
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+export async function deleteInvoice(id: string) { 
+  await sql`DELETE FROM invoices WHERE id = ${id}`; 
+  revalidatePath('/dashboard/invoices');
+} 
